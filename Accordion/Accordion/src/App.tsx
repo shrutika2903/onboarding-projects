305f50f363ce3108028d4e3b5732adac51343aad
shrutika <shrutikav29.com>
2023-06-16 11:27:54 +0530

    Fetching & passing the data, displaying the data & added icons

--- Accordion/Accordion/src/App.tsx
+++ Accordion/Accordion/src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import Question from './Question';
+import AccordionData from './AccordionData';
+import { Accordion } from './AccordionType';
+
 
 function App() {
-
+  const [questions, setQuestions] = useState(AccordionData);
+  console.log('Question', questions)
   return (
-   <>
-   </>
+    <div >
+      <h3>questions and answer about login</h3>
+      {
+        questions.map((question) => {
+          return (
+            <Question key={question.id} {...question} />
+          )
+        })
+      }
+    </div>
   )
 }
 
--- Accordion/Accordion/src/Question.tsx
+++ Accordion/Accordion/src/Question.tsx
@@ -1,4 +1,32 @@
-function Question() {
+import { useState } from "react";
+import AccordionData from "./AccordionData";
+import { Accordion } from './AccordionType';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { Button } from "@mui/material";
+
+interface AccordionProps {
+    id: number;
+    title: string;
+    info: string;
+}
+
+function Question(props: AccordionProps) {
+    const { title, info } = props;
+    const [showInfo, setShowInfo] = useState(false);
+
+    return (
+        <div className="question">
+            <header>
+                <h1>{title}</h1>
+                <Button className="btn" onClick={() => setShowInfo(!showInfo)}>
+                    {showInfo ? <RemoveIcon /> : <AddIcon />}
+                </Button>
+            </header>
+            {showInfo && <p>{info}</p>}
+        </div>
+
+    )
 
 }
 
